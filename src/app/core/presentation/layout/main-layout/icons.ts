9b47c065b40abef5c68b8749f025444639c8acb4
Cristhian Traversi <crist.trav@gmail.com>
2025-08-26 10:13:53 -0300

    feat(main-layout): crear menu principal con marcas y dashboard

--- src/app/core/presentation/layout/main-layout/icons.ts
+++ src/app/core/presentation/layout/main-layout/icons.ts
@@ -1,12 +1,11 @@
 import { IconDefinition } from '@ant-design/icons-angular';
-import { HomeOutline, MenuFoldOutline, MenuUnfoldOutline, TeamOutline, UserOutline } from '@ant-design/icons-angular/icons'
+import { HomeOutline, MenuFoldOutline, MenuUnfoldOutline, ProductOutline, TeamOutline, UserOutline } from '@ant-design/icons-angular/icons'
 
 const icons: IconDefinition[] = [
     MenuFoldOutline,
     MenuUnfoldOutline,
     HomeOutline,
-    UserOutline,
-    TeamOutline
+    ProductOutline
 ] as const;
 
 export default icons;
